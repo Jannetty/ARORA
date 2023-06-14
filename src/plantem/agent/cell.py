7b7747ca1d9da884b5c607bbe13d6cb6e9eecc6d
Jannetty <jannetty@uw.edu>
2023-06-14 12:41:52 -0700

    broke running, added perimeter to cell

--- src/plantem/agent/cell.py
+++ src/plantem/agent/cell.py
@@ -1,25 +1,37 @@
 import arcade
 from agent.circ_module import BaseCirculateModule
+from loc.quad_perimeter import QuadPerimeter
+from loc.vertex import Vertex
 
 class GrowingCell(arcade.Sprite):
 
-    corners = None
+    quad_perimeter = None
     circulator = None
     sim = None
 
     def __init__(self, simulation, corners: list):
         super().__init__()
-        self.corners = corners
+
+        perimeter = []
+        for corner in corners:
+            v = Vertex(corner[0], corner[1])
+            perimeter.append(v)
+        self.quad_perimeter = QuadPerimeter(perimeter)
+
         self.color = [0,200,5]
         circulator = BaseCirculateModule(self)
         sim = simulation
 
     def draw(self):
-        arcade.draw_polygon_filled(point_list=self.corners, color=self.color)
-        arcade.draw_polygon_outline(point_list=self.corners, color=[0,0,0])
+        arcade.draw_polygon_filled(point_list=self.quad_perimeter.get_corners(), color=self.color)
+        arcade.draw_polygon_outline(point_list=self.quad_perimeter.get_corners(), color=[0,0,0])
 
     def update(self):
-        for idx, corner in enumerate(self.corners):
+        corners = self.quad_perimeter.get_corners()
+        new_corners = []
+        for idx, corner in enumerate(corners):
+            new_corner = corner
             if idx == 1 or idx == 2:
-                new_corner = corner
                 new_corner[1] = corner[1] + .3
+            new_corners.append(new_corner)
+        self.quad_perimeter.set_corners(new_corners)
---
+++ src/plantem/loc/vertex.py
@@ -0,0 +1,7 @@
+class Vertex():
+    x = None
+    y = None 
+
+    def __init__(self, x:float, y:float):
+        self.x = x
+        self.y = y
