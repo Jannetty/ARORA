a4eb8fc72927d7bb2f94a4a13c9ce6351f13e55a
Jannetty <jannetty@uw.edu>
2023-06-15 14:57:14 -0700

    Continued to develop quad_perimeter. Test currently failing

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ File to run simulation
 
 if __name__ == '__main__':
     timestep = 1
-    sim.main(timestep)
+    root_midpoint_x = 400
+    sim.main(timestep, root_midpoint_x)
--- src/plantem/agent/cell.py
+++ src/plantem/agent/cell.py
@@ -8,6 +8,10 @@ class GrowingCell(arcade.Sprite):
     quad_perimeter = None
     circulator = None
     sim = None
+    a_neighbors = [] 
+    b_neighbors = []
+    l_neighbors = []
+    m_neighbors = []
 
     def __init__(self, simulation, corners: list, init_aux):
         super().__init__()
@@ -18,12 +22,47 @@ class GrowingCell(arcade.Sprite):
         circulator = BaseCirculateModule(self, init_aux)
         sim = simulation
 
-    def get_area(self):
+    def add_neighbor(self, cell: 'GrowingCell') -> None:
+        neighbor_location = self.find_new_neighbor_relative_location(cell)
+
+    def find_new_neighbor_relative_location(self, cell: 'GrowingCell') -> str:
+        sim_midpointx = self.sim.get_midpointx()
+
+    def get_a_neighbors(self):
+        return self.a_neighbors
+    def get_b_neighbors(self):
+        return self.b_neighbors
+    def get_m_neighbors(self):
+        return self.m_neighbors
+    def get_l_neighbors(self):
+        return self.l_neighbors
+
+
+    def remove_neighbor(self, cell: 'GrowingCell') -> None:
+        if isinstance(cell, self.a_neighbor):
+            self.a_neighbor.remove(cell)
+        if isinstance(cell, self.b_neighbor):
+            self.b_neighbor.remove(cell)
+        if isinstance(cell, self.l_neighbor):
+            self.l_neighbor.remove(cell)
+        if isinstance(cell, self.m_neighbor):
+            self.m_neighbor.remove(cell)
+
+    def get_area(self) -> float:
         return self.quad_perimeter.get_area()
 
-    def draw(self):
+    def draw(self) -> None:
         arcade.draw_polygon_filled(point_list=self.quad_perimeter.get_corners_for_disp(), color=self.color)
         arcade.draw_polygon_outline(point_list=self.quad_perimeter.get_corners_for_disp(), color=[0,0,0])
 
-    def update(self):
+    def grow(self) -> None:
         pass
+
+    def update(self) -> None:
+        new_bottom_left = self.quad_perimeter.get_bottom_left()
+        new_bottom_right = self.quad_perimeter.get_bottom_right()
+        old_top_left_xy = self.quad_perimeter.get_top_left().get_xy()
+        old_top_right_xy = self.quad_perimeter.get_top_right().get_xy()
+        new_top_left = Vertex(old_top_left_xy[0], old_top_left_xy[1]+.3)
+        new_top_right = Vertex(old_top_right_xy[0], old_top_right_xy[1]+.3)
+        self.quad_perimeter.set_corners([new_top_left,new_top_right, new_bottom_right, new_bottom_left])
