14af7ef497c2171450710bbe9e8d57a953bafcd6
Zimo135 <zhuzimo@uw.edu>
2023-06-15 14:13:37 -0700

    update circ_moudle

--- src/plantem/agent/circ_module.py
+++ src/plantem/agent/circ_module.py
@@ -2,20 +2,47 @@ class BaseCirculateModule():
 
     auxin = None
     ARR = None
-    ARRA = None
+    AUX_LAX = None
+    PINA = None
+    PINB = None
+    PINL = None
+    PINM = None
     cell = None
-    #
 
     def __init__(self, cell, init_vals):
         self.cell = cell
-        # initialize all valuesz
+        # initialize all values
         self.init_auxin = init_vals.get("auxin")
+        self.auxin = self.init_auxin
+
         self.init_ARR = init_vals.get("ARR")
+        self.ARR = self.init_ARR
+
         self.init_AUX_LAX = init_vals.get("AUX/LAX")
+        self.AUX_LAX = self.init_AUX_LAX
+
         self.init_PINA = init_vals.get("PINA")
+        self.PINA = self.init_PINA
+
         self.init_PINB = init_vals.get("PINB")
+        self.PINB = self.init_PINB
+
         self.init_PINL = init_vals.get("PINL")
+        self.PINL = self.init_PINL
+
         self.init_PINM = init_vals.get("PINM")
+        self.PINM = self.init_PINM
+
+        self.k_ARR_ARR = init_vals.get("k_ARR_ARR")
+        self.k_auxin_AUXLAX = init_vals.get("k_auxin_AUXLAX")
+        self.k_auxin_PIN = init_vals.get("k_auxin_PIN")
+        self.k_ARR_PIN = init_vals.get("k_ARR_PIN")
+
+        self.ks = init_vals.get("ks")
+        self.kd = init_vals.get("kd")
+        
+        self.timestep = 1
+        self.area = 100
 
     def update(self):
         pass
@@ -27,47 +54,78 @@ class BaseCirculateModule():
         cell_dict = curr_cell.sim.circulator.delta_auxins
 
         # base calculations
-        auxin = 1 - 1*self.init_auxin
-        ARR = 1 * (1/(self.init_ARR + 1) - 1*self.init_ARR)
-        AUX_LAX = 1*(auxin/(auxin+1))*(1/(ARR/1)+1) - 1*self.init_AUX_LAX
-        PIN = 1*(1/(ARR/1)+1)*(auxin/(auxin+1))
-        PINA = 0.25 * PIN - 1*self.init_PINA
-        PINB = 0.25 * PIN - 1*self.init_PINB
-        PINL = 0.25 * PIN - 1*self.init_PINL
-        PINM = 0.25 * PIN - 1*self.init_PINM
-        AuxinA = 1 * 0.25 * AUX_LAX - 1 * PINA
-        AuxinB = 1 * 0.25 * AUX_LAX - 1 * PINB
-        AuxinL = 1 * 0.25 * AUX_LAX - 1 * PINL
-        AuxinM = 1 * 0.25 * AUX_LAX - 1 * PINM
+        self.auxin = self.calculate_auxin(self.timestep, self.area)
+        self.ARR = self.calculate_ARR(self.timestep, self.area)
+        self.AUX_LAX = self.calculate_AUX_LAX(self.timestep, self.area)
+        self.PIN = self.calculate_PIN(self.timestep, self.area)
+        self.PINA = self.calculate_neighbor_PIN(self.init_PINA, self.timestep, self.area)
+        self.PINB = self.calculate_neighbor_PIN(self.init_PINB, self.timestep, self.area)
+        self.PINL = self.calculate_neighbor_PIN(self.init_PINL, self.timestep, self.area)
+        self.PINM = self.calculate_neighbor_PIN(self.init_PINM, self.timestep, self.area)
+        AuxinA = self.calculate_neighbor_auxin(self.init_PINA, self.timestep, self.area)
+        AuxinB = self.calculate_neighbor_auxin(self.init_PINB, self.timestep, self.area)
+        AuxinL = self.calculate_neighbor_auxin(self.init_PINL, self.timestep, self.area)
+        AuxinM = self.calculate_neighbor_auxin(self.init_PINM, self.timestep, self.area)
         neighbors_aux = [AuxinA, AuxinB, AuxinL, AuxinM]
 
-        # find neighbor
-        neighborA = curr_cell.neighborA
-        neighborB = curr_cell.neighborB
-        neighborL = curr_cell.neighborL
-        neighborM = curr_cell.neighborM
-        neighbors = [neighborA, neighborB, neighborL, neighborM]
+        # find neighbors
+        neighbors = self.find_neighbors(curr_cell)
 
         # update current cell
-        delta_aux = AuxinA + AuxinB + AuxinL + AuxinM
+        delta_aux = self.auxin + AuxinA + AuxinB + AuxinL + AuxinM
+        cell_dict = self.update_current_cell(curr_cell, cell_dict, delta_aux)
+
+        # update neighbor cells
+        cell_dict = self.update_neighbor_cell(cell_dict, neighbors, neighbors_aux)
+
+        return cell_dict
+
+    # Helper functions
+    def calculate_auxin(self, timestep, area) -> float:
+        auxin = (self.ks - self.kd*self.init_auxin*area) * timestep
+        return auxin
+
+    def calculate_ARR(self, timestep, area) -> float:
+        ARR = (self.ks * 1/(self.init_ARR/self.k_ARR_ARR + 1) - self.kd*self.init_ARR*area) * timestep
+        return ARR
+
+    def calculate_AUX_LAX(self, timestep, area) -> float:
+        auxin = self.calculate_auxin(timestep, area)
+        AUX_LAX = (self.ks*(auxin/(auxin+self.k_auxin_AUXLAX)) - self.kd*self.init_AUX_LAX*area) * timestep
+        return AUX_LAX
+
+    def calculate_PIN(self, timestep, area) -> float:
+        auxin = self.calculate_auxin(timestep, area)
+        ARR = self.calculate_ARR(timestep, area)
+        PIN = (self.ks * (1/(ARR/self.k_ARR_PIN + 1)) * (auxin/(auxin+self.k_auxin_PIN))) * timestep
+        return PIN
+
+    def calculate_neighbor_PIN(self, init, timestep, area) -> float:
+        PIN = self.calculate_PIN(timestep, area)
+        neighbor_PIN = (0.25 * PIN - self.kd*init*area) * timestep
+        return neighbor_PIN
+    
+    def calculate_neighbor_auxin(self, init, timestep, area) -> float:
+        AUX_LAX = self.calculate_AUX_LAX(timestep, area)
+        PIN = self.calculate_neighbor_PIN(init, timestep, area)
+        neighbor_auxin = (self.ks * 0.25 * AUX_LAX - self.kd * PIN * area) * timestep
+        return neighbor_auxin
+
+    def find_neighbors(self, curr_cell):
+        neighbors = [curr_cell.neighborA, curr_cell.neighborB, curr_cell.neighborL, curr_cell.neighborM]
+        return neighbors
+
+    def update_current_cell(self, curr_cell, cell_dict, delta_aux) -> dict:
         if curr_cell not in cell_dict:
             cell_dict[curr_cell] = delta_aux
         else:
             cell_dict[curr_cell] += delta_aux
-
-        # update neighbor cells
+        return cell_dict
+    
+    def update_neighbor_cell(self, cell_dict, neighbors, neighbors_aux) -> dict:
         for i in range(4):
             if neighbors[i] not in cell_dict:
-                cell_dict[neighbors[i]] = neighbors_aux[i]
+                cell_dict[neighbors[i]] = -neighbors_aux[i]
             else:
-                cell_dict[neighbors[i]] += neighbors_aux[i]
-
-
-def calculate_auxin(self, ks, kd, timestep, area):
-    auxin = (ks - kd*self.init_auxin*area) * timestep
-    return auxin
-
-
-def calculate_ARR(self, ks, kd):
-    ARR = ks * (1/(self.init_ARR + 1) - kd*self.init_ARR)
-    return ARR
+                cell_dict[neighbors[i]] += -neighbors_aux[i]
+        return cell_dict
--- tests/unit/plantem/test_circ_module_unittest.py
+++ tests/unit/plantem/test_circ_module_unittest.py
@@ -1,28 +1,100 @@
 import unittest
 
+from src.plantem.loc.vertex.vertex import Vertex
 from src.plantem.agent.circ_module import BaseCirculateModule
 from src.plantem.agent.cell import GrowingCell
 
 
 class BaseCirculateModuleTests(unittest.TestCase):
-    def test_update(self):
-        cell1 = GrowingCell(self, [[100.0,100.0], [100.0,300.0], [300.0,300.0], [300.0,100.0]], {})
+    # def test_update(self):
+    #     cell1 = GrowingCell(self, [Vertex(100.0,100.0), Vertex(100.0,300.0), Vertex(300.0,300.0), Vertex(300.0,100.0)], {})
 
-        test_dict = {cell1: 1}
+    #     test_dict = {cell1: 1}
 
-        for cell in test_dict:
-            with self.subTest(cell=cell):
-                circ_module = BaseCirculateModule(cell)
-                found_delta = circ_module.update()[cell]
-                expected_delta = test_dict[cell]
+    #     for cell in test_dict:
+    #         with self.subTest(cell=cell):
+    #             circ_module = BaseCirculateModule(cell,{})
+    #             found_delta = circ_module.update()[cell]
+    #             expected_delta = test_dict[cell]
 
-                self.assertEqual(found_delta, expected_delta)
+    #             self.assertEqual(found_delta, expected_delta)
+
+    init_vals = {"auxin": 2, "ARR": 3, "AUX/LAX": 3, "PINA": 0.5, "PINB": 0.7,
+                 "PINL": 0.4, "PINM": 0.2, "k_ARR_ARR": 1, "k_auxin_AUXLAX": 1,
+                 "k_auxin_PIN": 1, "k_ARR_PIN": 1, "ks": 0.005, "kd": 0.0015}
 
     def test_calculate_auxin(self):
+        circ_module = BaseCirculateModule(GrowingCell, self.init_vals)
         timestep = 1
         area = 100
-        
+        expected_auxin = -0.295
+        found_auxin = circ_module.calculate_auxin(timestep, area)
+        self.assertAlmostEqual(expected_auxin, found_auxin, places=5)
 
+    def test_calculate_ARR(self):
+        circ_module = BaseCirculateModule(GrowingCell, self.init_vals)
+        timestep = 1
+        area = 100
+        expected_ARR = -0.44875
+        found_ARR = circ_module.calculate_ARR(timestep, area)
+        self.assertAlmostEqual(expected_ARR, found_ARR, places=5)
+    
+    def test_calculate_AUX_LAX(self):
+        circ_module = BaseCirculateModule(GrowingCell, self.init_vals)
+        timestep = 1
+        area = 100
+        expected_AUX_LAX = -0.45209
+        found_AUX_LAX = circ_module.calculate_AUX_LAX(timestep, area)
+        self.assertAlmostEqual(expected_AUX_LAX, found_AUX_LAX, places=5)
+    
+    def test_calcualte_PIN(self):
+        circ_module = BaseCirculateModule(GrowingCell, self.init_vals)
+        timestep = 1
+        area = 100
+        expected_PIN = -0.0037954
+        found_PIN = circ_module.calculate_PIN(timestep, area)
+        self.assertAlmostEqual(expected_PIN, found_PIN, places=5)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_calculate_neighbor_PIN(self):
+        circ_module = BaseCirculateModule(GrowingCell, self.init_vals)
+        timestep = 1
+        area = 100
+        expected_neighbor_PIN = -0.075949
+        found_neighbor_PIN = circ_module.calculate_neighbor_PIN(0.5, timestep, area)
+        self.assertAlmostEqual(expected_neighbor_PIN, found_neighbor_PIN, places=5)
+
+    def test_calculate_neighbor_auxin(self):
+        circ_module = BaseCirculateModule(GrowingCell, self.init_vals)
+        timestep = 1
+        area = 100
+        expected_neighbor_auxin = 0.01082723
+        found_neighbor_auxin = circ_module.calculate_neighbor_auxin(0.5, timestep, area)
+        self.assertAlmostEqual(expected_neighbor_auxin, found_neighbor_auxin, places=5)
+
+    def test_update_current_cell(self):
+        circ_module = BaseCirculateModule(GrowingCell, self.init_vals)
+        cell_dict = {}
+        curr_cell = GrowingCell
+        delta_aux = 0.5
+
+        expected_dict = {curr_cell: 0.5}
+        found_dict = circ_module.update_current_cell(curr_cell, cell_dict, delta_aux)
+        self.assertEqual(expected_dict, found_dict)
+
+    def test_update_neighbor_cell(self):
+        circ_module = BaseCirculateModule(GrowingCell, self.init_vals)
+
+        curr_cell = GrowingCell
+        sim = None
+
+        cellA = GrowingCell(sim, [Vertex(100.0,100.0), Vertex(100.0,300.0), Vertex(300.0,300.0), Vertex(300.0,100.0)], self.init_vals)
+        cellB = GrowingCell(sim, [Vertex(101.0,101.0), Vertex(101.0,300.0), Vertex(300.0,300.0), Vertex(300.0,101.0)], self.init_vals)
+        cellL = GrowingCell(sim, [Vertex(100.0,100.0), Vertex(100.0,301.0), Vertex(301.0,301.0), Vertex(301.0,100.0)], self.init_vals)
+        cellM = GrowingCell(sim, [Vertex(200.0,200.0), Vertex(200.0,300.0), Vertex(300.0,300.0), Vertex(300.0,200.0)], self.init_vals)
+        neighbors = [cellA, cellB, cellL, cellM]
+        neighbors_aux = [0.1, -0.4, 0.05, 0]
+        cell_dict = {curr_cell: 0.5, cellA: 1}
+
+        expected_dict = {curr_cell: 0.5, cellA: 0.9, cellB: 0.4, cellL: -0.05, cellM: 0}
+        found_dict = circ_module.update_neighbor_cell(cell_dict, neighbors, neighbors_aux)
+        self.assertEqual(expected_dict, found_dict)
