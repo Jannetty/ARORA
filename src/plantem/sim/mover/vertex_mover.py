d05c572891d338c7a8e7efcdb7e3f398163ebe66
Jannetty <jannetty@uw.edu>
2023-06-20 15:56:30 -0700

    changes to vertex mover

--- src/plantem/sim/mover/vertex_mover.py
+++ src/plantem/sim/mover/vertex_mover.py
@@ -23,13 +23,17 @@ class VertexMover():
         return self.vertex_deltas[vertex]
 
     def update(self) -> None:
-        self.propogate_deltas()
+        top_row = self.get_top_row()
+        self.propogate_deltas(top_row)
         self.execute_vertex_movement()
         self.cell_deltas.clear()
         self.vertex_deltas.clear()
 
-    def propogate_deltas(self) -> None:
-        for cell in self.cell_deltas:
+    def get_top_row(self) -> list:
+        
+
+    def propogate_deltas(self, top_row : list) -> None:
+        for cell in top_row:
             this_delta = self.cell_deltas[cell]
             self.add_cell_b_vertices_to_vertex_deltas(cell, this_delta)
             self.recursively_propogate_deltas_to_b_neighbors(cell, this_delta)
@@ -38,18 +42,19 @@ class VertexMover():
         bottom_left_v = cell.get_quad_perimeter().get_bottom_left()
         bottom_right_v = cell.get_quad_perimeter().get_bottom_right()
         if bottom_left_v in self.vertex_deltas:
-            self.vertex_deltas[bottom_left_v] = self.vertex_deltas[bottom_left_v] + delta
+            pass
         else:
             self.vertex_deltas[bottom_left_v] = delta
         if bottom_right_v in self.vertex_deltas:
-            self.vertex_deltas[bottom_right_v] = self.vertex_deltas[bottom_right_v] + delta
+            pass
         else:
             self.vertex_deltas[bottom_right_v] = delta
 
     def recursively_propogate_deltas_to_b_neighbors(self, cell : GrowingCell, delta : float) -> None:
         for b_neighbor in cell.get_b_neighbors():
-            self.add_cell_b_vertices_to_vertex_deltas(b_neighbor, delta)
-            self.recursively_propogate_deltas_to_b_neighbors(b_neighbor, delta)
+            neighbor_delta = self.cell_deltas[b_neighbor]
+            self.add_cell_b_vertices_to_vertex_deltas(b_neighbor, delta + neighbor_delta)
+            self.recursively_propogate_deltas_to_b_neighbors(b_neighbor, delta + neighbor_delta)
 
     def execute_vertex_movement(self) -> None:
         for vertex in self.vertex_deltas:
