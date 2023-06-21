58b4810c2cf4b0a50650f5ec237cab6e28d9f5bf
Zimo135 <zhuzimo@uw.edu>
2023-06-21 16:43:14 -0700

    updates

--- src/plantem/sim/simulation/sim.py
+++ src/plantem/sim/simulation/sim.py
@@ -50,7 +50,9 @@ class GrowingSim(arcade.Window):
                 Vertex(300.0, 300.0),
                 Vertex(300.0, 100.0),
             ],
-            1,
+            {"auxin": 2, "arr": 3, "aux_lax": 3, "pina": 0.5, "pinb": 0.7,
+             "pinl": 0.4, "pinm": 0.2, "k_arr_arr": 1, "k_auxin_auxlax": 1,
+             "k_auxin_pin": 1, "k_arr_pin": 1, "ks": 0.005, "kd": 0.0015},
         )
         self.cell_list.append(this_cell)
 
