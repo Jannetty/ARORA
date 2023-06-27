9209acd6e57bf72ca0d25c41f0698bedcc59c514
Jannetty <jannetty@uw.edu>
2023-06-27 14:21:03 -0700

    added add_delta function to circulator

--- src/plantem/sim/circulator/circulator.py
+++ src/plantem/sim/circulator/circulator.py
@@ -2,4 +2,12 @@ class Circulator:
     delta_auxins = None
 
     def __init__(self):
-        delta_auxins = dict()
+        self.delta_auxins = dict()
+
+    def add_delta(self, cell, delta) -> None :
+        if cell in self.delta_auxins:
+            old_delta = self.delta_auxins[cell]
+            new_delta = old_delta + delta
+            self.delta_auxins[cell] = new_delta
+        else:
+            self.delta_auxins[cell] = delta
