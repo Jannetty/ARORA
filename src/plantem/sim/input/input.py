8bacec8a188e1698bf1553134a29c10a833638c7
Zimo135 <zhuzimo@uw.edu>
2023-06-30 14:46:13 -0700

    update input class

--- src/plantem/sim/input/input.py
+++ src/plantem/sim/input/input.py
@@ -12,11 +12,25 @@ class Input:
         self.init_vals_input = pandas.read_csv(init_vals_file)
         self.vertex_input = pandas.read_csv(vertex_file)
 
-    def get_vertex(self):
-        pass
+    def get_vertex(self) -> dict:
+        vertex_dict = dict()
+        for index, row in self.vertex_input.iterrows():
+            vertex_dict["{0}".format(index)] = row.to_dict()
+        # update the vertex_dict to store vertcies in Vertex format
+        for each in vertex_dict:
+            x = vertex_dict[each]["x"]
+            y = vertex_dict[each]["y"]
+            vertex_dict[each] = Vertex(x, y)
+        return vertex_dict
 
-    def get_init_vals(self):
-        pass
+    def get_init_vals(self) -> dict:
+        init_vals_dict = dict()
+        for index, row in self.init_vals_input.iloc[:, :15].iterrows():
+            init_vals_dict["init_vals_set{0}".format(index)] = row.to_dict()
+        return init_vals_dict
+
+    # def get_vertex_assignment(self) -> dict:
+    #     vertex_assign = dict()
 
     def create_cell(self):
         pass
