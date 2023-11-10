0fd461b0086905675b915693ccfd9f9f070d5e59
Sophia Jannetty <skjannetty@Sophias-MacBook-Pro-4.local>
2023-11-10 09:39:24 -0800

    added docstrings to circulator class

--- src/plantem/sim/circulator/circulator.py
+++ src/plantem/sim/circulator/circulator.py
@@ -1,14 +1,47 @@
 class Circulator:
+    """
+    Circulator manages the delta auxins in a simulation. Each time step cells save their delta auxin to the circulator
+    and delta auxins of that cell's neighbors are updated. At the end of the time step, the circulator updates the auxin
+
+    Attributes:
+        delta_auxins: A dictionary to store the delta auxins for each cell. Keys are cells, values are delta auxins.
+        sim: The simulation that this circulator is part of.
+    """
     delta_auxins = None
 
     def __init__(self, sim):
+        """
+        Constructs a new Circulator instance.
+
+        Args:
+            sim: The simulation that this circulator is part of.
+        """
         self.delta_auxins = dict()
         self.sim = sim
 
     def get_delta_auxins(self) -> dict:
+        """
+        Returns the dictionary of delta auxins that the circulator is managing. 
+        Keys are cells, values are delta auxins.
+
+        Returns:
+            dict: The dictionary of delta auxins.
+        """
         return self.delta_auxins
 
     def add_delta(self, cell, delta) -> None:
+        """
+        Adds a delta auxin to the circulator for a given cell. If the cell is already in the circulator,
+        the delta auxin is added to the existing delta auxin. If the cell is not in the circulator, the 
+        delta auxin is added to the circulator.
+
+        Args:
+            cell: The cell that the delta auxin is for.
+            delta: The delta auxin to add.
+
+        Returns:
+            None
+        """
         if cell in self.delta_auxins:
             old_delta = self.delta_auxins[cell]
             new_delta = old_delta + delta
@@ -21,6 +54,13 @@ class Circulator:
             self.delta_auxins[cell] = delta
 
     def update(self) -> None:
+        """
+        Updates the auxin of each cell in the circulator by adding the delta auxin to each cell's
+        existing auxin.
+
+        Returns:
+            None
+        """
         for cell in self.delta_auxins:
             if cell.id == 0 or cell.id == 1:
                 print(f"cell {cell.get_id()} aux before = {cell.get_circ_mod().get_auxin()}")
