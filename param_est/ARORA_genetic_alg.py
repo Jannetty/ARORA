6f8a149414d0e72174c9f93051b78b6c3ebce286
Sophia Jannetty <skjannetty@Sophias-MacBook-Pro-5.local>
2024-03-01 14:47:52 -0800

    added make_paramspace function:wq

--- param_est/ARORA_genetic_alg.py
+++ param_est/ARORA_genetic_alg.py
@@ -20,13 +20,13 @@ class ARORAGeneticAlg:
         self.parameters_df = pd.read_csv(initial_parent_gen_file)
         self.ga_instance = None
 
-    def fitness_function(self, solution, solution_idx):
+    def fitness_function(self, ga_instance, solution, solution_idx):
         params = self.parameters_df.iloc[solution_idx]
-        cost = self.run_ARORA(params)
+        cost = self._run_ARORA(params)
         fitness = -cost
         return fitness
     
-    def run_ARORA(self, params):
+    def _run_ARORA(self, params):
         timestep = 1
         root_midpoint_x = 71
         vis = False
@@ -49,24 +49,37 @@ class ARORAGeneticAlg:
         simulation.setup()
         try:
             simulation.run_sim()
-            cost = self.calculate_cost(simulation)
+            cost = self._calculate_cost(simulation)
         except Exception as e:
             print("Cost set to infinity")
             cost = np.inf
         return cost
     
-    def calculate_cost(self, simulation):
+    def _calculate_cost(self, simulation):
         # calculate cost
         cost = auxin_greater_in_larger_cells(simulation) + auxin_peak_at_root_tip(simulation)
         return cost
 
-    def run_ga(self):
-        self.ga_instance = pygad.GA(num_generations=50,
+    def make_paramspace(self):
+        ks_range = np.linspace(0.001, 0.3, 10)
+        kd_range = np.linspace(0.0001, 0.03, 10)
+        k1_range = np.round(np.linspace(10, 160, 10)).astype(int)
+        k2_range = np.round(np.linspace(50, 100, 10)).astype(int)
+        k3_range = np.round(np.linspace(10, 75, 10)).astype(int)
+        k4_range = np.round(np.linspace(50, 100, 10)).astype(int)
+        k5_range = np.linspace(0.07, 20, 10) # kal
+        k6_range = np.linspace(0.2, 20, 10) # kpin
+        tau_range = np.round(np.linspace(60, 7200, 10)).astype(int)
+        return [ks_range, kd_range, k1_range, k2_range, k3_range, k4_range, k5_range, k6_range, tau_range]
+
+    def run_genetic_alg(self):
+        genespace = self.make_paramspace()
+        self.ga_instance = pygad.GA(num_generations=1,
                                     num_parents_mating=10,
                                     fitness_func=self.fitness_function,
                                     sol_per_pop=len(self.parameters_df),
                                     num_genes=len(self.parameters_df.columns),
-                                    gene_space=[list(range(len(self.parameters_df)))] * len(self.parameters_df.columns),  # Assuming each parameter can take any row index value
+                                    gene_space=genespace,
                                     mutation_percent_genes=50)
 
         self.ga_instance.run()
