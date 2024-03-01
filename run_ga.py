db1dea6fbf44a6b56237d7037d96ab0e60e3969a
Sophia Jannetty <skjannetty@Sophias-MacBook-Pro-5.local>
2024-03-01 14:47:52 -0800

    added make_paramspace function:wq

--- run_ga.py
+++ run_ga.py
@@ -3,5 +3,5 @@ from param_est.ARORA_genetic_alg import ARORAGeneticAlg
 if __name__ == "__main__":
     init_parent_file = "param_est/test_first_gen.csv"
     ga = ARORAGeneticAlg(init_parent_file)
-    print(ga.fitness_function(None, 0))
+    ga.run_genetic_alg()
     exit(0)
